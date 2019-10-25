02e0b6e1e316a526bdf600ca16fc9aedd2df65da
Filipp Berzenin <filipp.berzenin@gmail.com>
2019-10-25 21:38:13 +0300

    add function add row

--- src/main/webapp/js/shop_properties.js
+++ src/main/webapp/js/shop_properties.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-	let i = 1;
 	
 	$('#shopProperties').find(".custom-control-input").each(function() {
 		$(this).prop('checked', true)
@@ -15,29 +14,27 @@ $(document).ready(function() {
 			}
 	});
 	
-	function blockNotUsedInput (object) {
-		if ($(object).is(':checked')) {
-			$(object).closest('tr').children().children('[id $= input]').prop('disabled', true);
-			$(object).closest('tr').children().children('[type=text][id *= excel]').prop('disabled', false);
-		} else if (!$(object).is(':checked')) {
-			$(object).closest('tr').children().children('[id $= input]').prop('disabled', false);
-			$(object).closest('tr').children().children('[type=text][id *= excel]').prop('disabled', true);
+//	$('table').each(function() {
+//		$(this).each().css('background', 'red');
+//	});
+	
+	find(".custom-control-input").each(function() {
+		if ($(this).is(':checked')) {
+			$('[id $= input]').prop('disabled', true);
+			$('[id *= excel]').prop('disabled', false);
+		} else if (!$(this).is(':checked')) {
+			$('[id $= input]').prop('disabled', false);
+			$('[id *= excel]').prop('disabled', true);
+		}
+	});
+	
+	$(document).on('change', '.custom-control-input', function() {
+		if ($(this).is(':checked')) {
+			$(this).closest('tr').children().children('[id $= input]').prop('disabled', true);
+			$(this).closest('tr').children().children('[type=text][id *= excel]').prop('disabled', false);
+		} else if (!$(this).is(':checked')) {
+			$(this).closest('tr').children().children('[id $= input]').prop('disabled', false);
+			$(this).closest('tr').children().children('[type=text][id *= excel]').prop('disabled', true);
 		}	
-	}
-	
-	$(document).on('change', '#excelResourcesName', function() {
-		blockNotUsedInput(this)
-	});
-	
-	$(document).on('change', '#excelResourcesCompany', function() {
-		blockNotUsedInput(this);
-	});
-	
-	$(document).on('change', '#excelResourcesUrl', function() {
-		blockNotUsedInput(this);
-	});
-	
-	$(document).on('change', '#excelResourcesPlatform', function() {
-		blockNotUsedInput(this);
 	});
 });
